c8ccea43af2a0ffad0732c01119264565c41744a
shoichi hatanaka <shoichi@toy-box.westlab>
2021-11-07 17:53:17 +0900

    display image

---
+++ opencv/displayImage/build/CMakeFiles/displayImage.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/displayImage.dir/displayImage.cpp.o"
+  "CMakeFiles/displayImage.dir/displayImage.cpp.o.d"
+  "displayImage"
+  "displayImage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/displayImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ opencv/displayImage/displayImage.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<stdio.h>
+#include<opencv2/opencv.hpp>
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        std::cerr << "Usage: ./displayImage.out [image_path]" << std::endl;
+        return 1;
+    }
+
+    cv::Mat image;
+    image = cv::imread(argv[1], 1);
+
+    if(!image.data)
+    {
+        std::cerr << "No Image Data" << std::endl;
+        return 1;
+    }
+
+    cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Image", image);
+
+    cv::waitKey(0);
+
+    return 0;
+}
