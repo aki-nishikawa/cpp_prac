130ba7a8798384e59d5682ab6810c86be1ac5cb6
Akihito Nishikawa <akihito.n.0910@keio.jp>
2022-01-29 16:19:19 +0900

    modified

--- general/1_grammar/function.cpp
+++ general/1_grammar/function.cpp
@@ -6,7 +6,8 @@ int Add(int a, int b);
 int main()
 {
     int a, b;
-    cin >> a >> b;
+    cout << "a = ? "; cin >> a;
+    cout << "b = ? "; cin >> b;
 
     cout << "a + b = " << Add(a, b) << endl;
 }
