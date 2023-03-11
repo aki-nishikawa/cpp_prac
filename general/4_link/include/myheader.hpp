330e5d8a50401ababb0f66aaa43649e2e2c29428
aki-nishikawa <akihito.n.0910@keio.jp>
2023-03-11 21:58:30 +0900

    feat: split file and library

--- general/4_link/include/myheader.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef MY_HEADER_
-#define MY_HEADER_
-
-void hello();
-int Add(int a, int b);
-int Sub(int a, int b);
-
-#endif
--- general/4_link/src/add_sub.cpp
+++
@@ -1,9 +0,0 @@
-// add b to a
-int Add(int a, int b){
-    return a + b;
-}
-
-// substruct b from a
-int Sub(int a, int b){
-    return a - b;
-}
