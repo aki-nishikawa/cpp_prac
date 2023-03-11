19bb27aa9c037c85a09719b13120ec17ca3a2006
aki-nishikawa <akihito.n.0910@keio.jp>
2023-03-11 21:32:41 +0900

    fix: rename

--- general/4_lib/1_one_file/src/main.cpp
+++ general/4_lib/1_one_file/src/main.cpp
@@ -6,22 +6,22 @@ void hello(){
 }
 
 // add b to a
-int Add(int a, int b){
+int add(int a, int b){
     return a + b;
 }
 
 // substruct b from a
-int Sub(int a, int b){
+int sub(int a, int b){
     return a - b;
 }
 
 int main(int argc, char *argv[]){
 
     int a = 5, b = 3;
-    int add = Add(a, b);
-    int sub = Sub(a, b);
+    int ans_add = add(a, b);
+    int ans_sub = sub(a, b);
 
     hello();
-    std::cout << a << " + " << b << " = " << add << std::endl;
-    std::cout << a << " - " << b << " = " << sub << std::endl;
+    std::cout << a << " + " << b << " = " << ans_add << std::endl;
+    std::cout << a << " - " << b << " = " << ans_sub << std::endl;
 }
